274039e455d0cd698e6008dc1a9a4a992e255042
Forsyth Creations <robert.h.forsyth@gmail.com>
2024-06-29 17:50:22 -0400

    All changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -161,12 +161,12 @@
  *          TMC5160, TMC5160_STANDALONE
  * :['A4988', 'A5984', 'DRV8825', 'LV8729', 'TB6560', 'TB6600', 'TMC2100', 'TMC2130', 'TMC2130_STANDALONE', 'TMC2160', 'TMC2160_STANDALONE', 'TMC2208', 'TMC2208_STANDALONE', 'TMC2209', 'TMC2209_STANDALONE', 'TMC2660', 'TMC2660_STANDALONE', 'TMC5130', 'TMC5130_STANDALONE', 'TMC5160', 'TMC5160_STANDALONE']
  */
-#define X_DRIVER_TYPE  TMC2209_STANDALONE 
-#define Y_DRIVER_TYPE  TMC2209_STANDALONE 
-#define Z_DRIVER_TYPE  TMC2209_STANDALONE 
+#define X_DRIVER_TYPE  TMC2209 
+#define Y_DRIVER_TYPE  TMC2209 
+#define Z_DRIVER_TYPE  TMC2209 
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
-#define Z2_DRIVER_TYPE TMC2209_STANDALONE 
+#define Z2_DRIVER_TYPE TMC2209 
 //#define Z3_DRIVER_TYPE A4988
 //#define Z4_DRIVER_TYPE A4988
 //#define I_DRIVER_TYPE  A4988
@@ -1257,7 +1257,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 810 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 795 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
@@ -1602,7 +1602,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40, 2, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 40, 2, -3.3 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
